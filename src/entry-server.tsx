8a6456436870a202d43455021640cc56badc6e09
preetisharma-ACL <preetiaajneeti@gmail.com>
2026-07-17 12:35:49 +0530

    added gtag and analytics

--- src/entry-server.tsx
+++ src/entry-server.tsx
@@ -8,8 +8,22 @@ export default createHandler(() => (
         <head>
           <meta charset="utf-8" />
           <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <meta
+            name="google-site-verification"
+            content="pozMz1gV_tRrQWj3sI8dFa8khjsy0MOOjgTCfvVu828"
+          />
           <link rel="icon" type="image/png" href="/logo/aajneeti-favicon.png" />
           <link rel="apple-touch-icon" href="/logo/aajneeti-favicon.png" />
+
+          {/* Google tag (gtag.js) — site-wide analytics */}
+          <script async src="https://www.googletagmanager.com/gtag/js?id=G-DJCMEPXJS2" />
+          <script
+            innerHTML={`window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+
+gtag('config', 'G-DJCMEPXJS2');`}
+          />
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link
             rel="preconnect"
