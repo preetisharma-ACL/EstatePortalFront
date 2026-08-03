5553f2c786b44f18b74d2b1cf08ed7633b531b11
preetisharma-ACL <preetiaajneeti@gmail.com>
2026-08-03 16:02:02 +0530

    hide sitemap

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -64,7 +64,7 @@ export default function Footer() {
               <FooterCol title="Company" links={[
                 { href: "/why-estateportal", label: "Why EstatePortal" },
                 { href: "/#enquire", label: "Talk to an advisor" },
-                { href: "/sitemap.xml", label: "Sitemap" },
+                // { href: "/sitemap.xml", label: "Sitemap" },
               ]} />
             </div>
           </div>
